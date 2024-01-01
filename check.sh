1dd0d74d31b4aac250a31468274db64ec26e58c7
Minhyeok Park <pmh_only@pmh.codes>
2024-01-01 21:10:16 +0900

    fix: typo

--- check.sh
+++ check.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 PREVIOUS_VERSION=$(cat latest)
-CURRENT_VERSION=$(curl https://api.github.com/repos/Anuken/Mindustry/releases/latest | jq ".tag_name")
+CURRENT_VERSION=$(curl https://api.github.com/repos/Anuken/Mindustry/releases/latest | jq ".tag_name" -r)
 
 [[ $PREVIOUS_VERSION != $CURRENT_VERSION ]]
 
-echo "VERSION_UPDATED=$?" >> $GITHUB_OUTPUT
-echo "VERSION=$CURRENT_VERSION" >> $GITHUB_OUTPUT
+echo "VERSION_UPDATED=$?" >> "$GITHUB_OUTPUT"
+echo "VERSION=$CURRENT_VERSION" >> "$GITHUB_OUTPUT"
 
 echo $CURRENT_VERSION > latest
